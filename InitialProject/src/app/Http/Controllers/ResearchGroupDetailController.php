2773d82cdaf308c0f3a4d225d95f2ebab6741dca
Witchakorn Boonprakom <witchakorn.b@kkumail.com>
2025-02-11 22:04:54 +0700

    feat(ResearchGroup): Add link column to research_groups table and update controller logic for link handling

--- InitialProject/src/app/Http/Controllers/ResearchGroupDetailController.php
+++ InitialProject/src/app/Http/Controllers/ResearchGroupDetailController.php
@@ -12,30 +12,22 @@ class ResearchGroupDetailController extends Controller
 {
     public function request($id)
     {
-        $resgd = ResearchGroup::with(['User.paper' => function ($query) {
+        // ดึงข้อมูลกลุ่มวิจัยพร้อมความสัมพันธ์ที่ต้องการ
+        $researchGroup = ResearchGroup::with(['User.paper' => function ($query) {
             return $query->orderBy('paper_yearpub', 'DESC');
-        }])->where('id', '=', $id)->get();
+        }])->findOrFail($id);
 
-        //return $resgd;
-        // $std = ResearchGroup::hasRole('student')::with(['User.paper' => function ($query) {
-        //     return $query->orderBy('paper_yearpub','DESC');
-        // }])->where('id','=',$id)->get();
-        // $ref = $resgd[0]->user[1]->fname_en;
-        // $rel = $resgd[0]->user[1]->lname_en;
-        // $author = Author::where([['author_fname', '=', $ref], ['author_lname', '=', $rel]])->get();
-        //return  $author;
+        // ตรวจสอบว่ามีค่า link หรือไม่ ถ้ามีให้ re‑direct ไปยัง URL นั้น
+        if (!empty($researchGroup->link)) {
+            return redirect()->away($researchGroup->link);
+        }
 
-        // $author = Paper::whereHas('author', function($q){
-        //     $q->where('author_fname', '=', 'Pongsathon');
-        // })->get();
-        // $author = collect($author);
-        //return  $author;
-
-        return view('researchgroupdetail', compact('resgd'));
-        //return $resgd;
-
+        // หากไม่มี link ส่งข้อมูลออกไปยัง view โดยห่อด้วย collection เพื่อให้เข้ากับการวนลูปใน view เดิม
+        return view('researchgroupdetail', ['resgd' => collect([$researchGroup])]);
     }
 
+    // ฟังก์ชัน user() ด้านล่างนี้ดูเหมือนจะไม่ใช่ส่วนที่เกี่ยวข้องกับ Controller
+    // ควรอยู่ใน Model แต่หากยังคงใช้งานก็สามารถไว้ได้
     public function user()
     {
         return $this->belongsToMany(User::class, 'work_of_research_groups')
--- InitialProject/src/resources/views/research_groups/edit.blade.php
+++ InitialProject/src/resources/views/research_groups/edit.blade.php
@@ -15,127 +15,154 @@
         <div class="card-body">
             <h4 class="card-title">แก้ไขข้อมูลกลุ่มวิจัย</h4>
             <p class="card-description">กรอกข้อมูลแก้ไขรายละเอียดกลุ่มวิจัย</p>
-            <form action="{{ route('researchGroups.update',$researchGroup->id) }}" method="POST" enctype="multipart/form-data" >
+            <form action="{{ route('researchGroups.update', $researchGroup->id) }}" method="POST" enctype="multipart/form-data">
                 @csrf
                 @method('PUT')
+                
+                <!-- ชื่อกลุ่มวิจัย (ภาษาไทย) -->
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>ชื่อกลุ่มวิจัย (ภาษาไทย)</b></p>
+                    <p class="col-sm-3"><b>ชื่อกลุ่มวิจัย (ภาษาไทย)</b></p>
                     <div class="col-sm-8">
                         <input name="group_name_th" value="{{ $researchGroup->group_name_th }}" class="form-control"
                             placeholder="ชื่อกลุ่มวิจัย (ภาษาไทย)">
                     </div>
                 </div>
+                
+                <!-- ชื่อกลุ่มวิจัย (English) -->
                 <div class="form-group row">
-                    <p class="col-sm-3 "><b>ชื่อกลุ่มวิจัย (English)</b></p>
+                    <p class="col-sm-3"><b>ชื่อกลุ่มวิจัย (English)</b></p>
                     <div class="col-sm-8">
                         <input name="group_name_en" value="{{ $researchGroup->group_name_en }}" class="form-control"
                             placeholder="ชื่อกลุ่มวิจัย (English)">
                     </div>
                 </div>
+                
+                <!-- คำอธิบายกลุ่มวิจัย (ภาษาไทย) -->
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>คำอธิบายกลุ่มวิจัย (ภาษาไทย)</b></p>
+                    <p class="col-sm-3"><b>คำอธิบายกลุ่มวิจัย (ภาษาไทย)</b></p>
                     <div class="col-sm-8">
-                        <textarea name="group_desc_th" value="{{ $researchGroup->group_desc_th }}" class="form-control"
-                            style="height:90px">{{ $researchGroup->group_desc_th }}</textarea>
+                        <textarea name="group_desc_th" class="form-control" style="height:90px">{{ $researchGroup->group_desc_th }}</textarea>
                     </div>
                 </div>
+                
+                <!-- คำอธิบายกลุ่มวิจัย (English) -->
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>คำอธิบายกลุ่มวิจัย (English)</b></p>
+                    <p class="col-sm-3"><b>คำอธิบายกลุ่มวิจัย (English)</b></p>
                     <div class="col-sm-8">
-                        <textarea name="group_desc_en" value="{{ $researchGroup->group_desc_en }}" class="form-control"
-                            style="height:90px">{{ $researchGroup->group_desc_en }}</textarea>
+                        <textarea name="group_desc_en" class="form-control" style="height:90px">{{ $researchGroup->group_desc_en }}</textarea>
                     </div>
                 </div>
+                
+                <!-- รายละเอียดกลุ่มวิจัย (ภาษาไทย) -->
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>รายละเอียดกลุ่มวิจัย (ภาษาไทย)</b></p>
+                    <p class="col-sm-3"><b>รายละเอียดกลุ่มวิจัย (ภาษาไทย)</b></p>
                     <div class="col-sm-8">
-                        <textarea name="group_detail_th" value="{{ $researchGroup->group_detail_th }}" class="form-control"
-                            style="height:90px">{{ $researchGroup->group_detail_th }}</textarea>
+                        <textarea name="group_detail_th" class="form-control" style="height:90px">{{ $researchGroup->group_detail_th }}</textarea>
                     </div>
                 </div>
+                
+                <!-- รายละเอียดกลุ่มวิจัย (English) -->
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>รายละเอียดกลุ่มวิจัย (English)</b></p>
+                    <p class="col-sm-3"><b>รายละเอียดกลุ่มวิจัย (English)</b></p>
                     <div class="col-sm-8">
-                        <textarea name="group_detail_en" value="{{ $researchGroup->group_detail_en }}" class="form-control"
-                            style="height:90px">{{ $researchGroup->group_detail_en }}</textarea>
+                        <textarea name="group_detail_en" class="form-control" style="height:90px">{{ $researchGroup->group_detail_en }}</textarea>
                     </div>
                 </div>
+                
+                <!-- Image -->
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>image</b></p>
+                    <p class="col-sm-3"><b>Image</b></p>
                     <div class="col-sm-8">
-                        <input type="file" name="group_image" class="form-control" >
+                        <input type="file" name="group_image" class="form-control">
                     </div>
                 </div>
+                
+                <!-- Link (ถ้ามี) -->
                 <div class="form-group row">
-                    <p class="col-sm-3"><b>หัวหน้ากลุ่มวิจัย</b></p>
+                    <p class="col-sm-3"><b>Link (ถ้ามี)</b></p>
                     <div class="col-sm-8">
-                        <select id='head0' name="head">
+                        <input name="link" type="url" value="{{ old('link', $researchGroup->link) }}" class="form-control" placeholder="https://example.com">
+                    </div>
+                </div>
+                
+                <!-- หัวหน้ากลุ่มวิจัย -->
+                <div class="form-group row">
+                    <p class="col-sm-3"><b>หัวหน้ากลุ่มวิจัย</b></p>
+                    <div class="col-sm-8">
+                        <select id="head0" name="head">
                             @foreach($researchGroup->user as $u)
-                            @if($u->pivot->role == 1)
-                            @foreach($users as $user)
-                            <option value="{{ $user->id }}" @if($u->id == $user->id) selected @endif>
-                                {{ $user->fname_th }} {{ $user->lname_th }}
-                            </option>
-                            @endforeach
-                            @endif
+                                @if($u->pivot->role == 1)
+                                    @foreach($users as $user)
+                                        <option value="{{ $user->id }}" @if($u->id == $user->id) selected @endif>
+                                            {{ $user->fname_th }} {{ $user->lname_th }}
+                                        </option>
+                                    @endforeach
+                                @endif
                             @endforeach
                         </select>
                     </div>
                 </div>
+                
+                <!-- สมาชิกกลุ่มวิจัย -->
                 <div class="form-group row">
                     <p class="col-sm-3 pt-4"><b>สมาชิกกลุ่มวิจัย</b></p>
                     <div class="col-sm-8">
                         <table class="table" id="dynamicAddRemove">
                             <tr>
-                                <th><button type="button" name="add" id="add-btn2" class="btn btn-success btn-sm add"><i
-                                            class="mdi mdi-plus"></i></button></th>
+                                <th>
+                                    <button type="button" name="add" id="add-btn2" class="btn btn-success btn-sm add">
+                                        <i class="mdi mdi-plus"></i>
+                                    </button>
+                                </th>
                             </tr>
                         </table>
                     </div>
                 </div>
                 
+                <!-- ปุ่ม Submit -->
                 <button type="submit" class="btn btn-primary mt-5">Submit</button>
-                <a class="btn btn-light mt-5" href="{{ route('researchGroups.index') }}"> Back</a>
+                <a class="btn btn-light mt-5" href="{{ route('researchGroups.index') }}">Back</a>
             </form>
         </div>
     </div>
 </div>
 @stop
+
 @section('javascript')
 <script>
 $(document).ready(function() {
-    $("#head0").select2()
-    $("#fund").select2()
-
+    $("#head0").select2();
+    $("#fund").select2();
 
     var researchGroup = <?php echo $researchGroup['user']; ?>;
     var i = 0;
 
+    // วนลูปแสดงสมาชิกกลุ่มที่มี role = 2 (สมาชิก)
     for (i = 0; i < researchGroup.length; i++) {
         var obj = researchGroup[i];
 
         if (obj.pivot.role === 2) {
             $("#dynamicAddRemove").append('<tr><td><select id="selUser' + i + '" name="moreFields[' + i +
-                '][userid]"  style="width: 200px;">@foreach($users as $user)<option value="{{ $user->id }}" >{{ $user->fname_th }} {{ $user->lname_th }}</option>@endforeach</select></td><td><button type="button" class="btn btn-danger btn-sm remove-tr"><i class="mdi mdi-minus"></i></button></td></tr>'
+                '][userid]"  style="width: 200px;">@foreach($users as $user)<option value="{{ $user->id }}">{{ $user->fname_th }} {{ $user->lname_th }}</option>@endforeach</select></td><td><button type="button" class="btn btn-danger btn-sm remove-tr"><i class="mdi mdi-minus"></i></button></td></tr>'
             );
             document.getElementById("selUser" + i).value = obj.id;
-            $("#selUser" + i).select2()
-
+            $("#selUser" + i).select2();
         }
-        //document.getElementById("#dynamicAddRemove").value = "10";
     }
+    
+    // ฟังก์ชันเพิ่มสมาชิกใหม่ในตาราง
     $("#add-btn2").click(function() {
         ++i;
         $("#dynamicAddRemove").append('<tr><td><select id="selUser' + i + '" name="moreFields[' + i +
             '][userid]"  style="width: 200px;"><option value="">Select User</option>@foreach($users as $user)<option value="{{ $user->id }}">{{ $user->fname_th }} {{ $user->lname_th }}</option>@endforeach</select></td><td><button type="button" class="btn btn-danger btn-sm remove-tr"><i class="mdi mdi-minus"></i></button></td></tr>'
         );
-        $("#selUser" + i).select2()
-
+        $("#selUser" + i).select2();
     });
+    
+    // ฟังก์ชันลบสมาชิก
     $(document).on('click', '.remove-tr', function() {
         $(this).parents('tr').remove();
     });
-
 });
 </script>
 @stop
